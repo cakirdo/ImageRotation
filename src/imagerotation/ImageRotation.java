8c54cad1c1573dad3bc978cb24851553dc52bb6d
dogan.cakir <dogan.cakir@DOGAN-CAKIR-SADE.lan>
2017-03-16 10:35:46 +0200

    re-indented

--- src/imagerotation/ImageRotation.java
+++ src/imagerotation/ImageRotation.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package imagerotation;
 
 import java.awt.FlowLayout;
@@ -20,56 +20,42 @@ import javax.swing.JLabel;
  * @author dogan.cakir
  */
 public class ImageRotation {
-
+    
     public static double rotatedX(int x,int y,double degree)
     {
         return Math.cos(Math.toRadians(degree))*x-Math.sin(Math.toRadians(degree))*y;
     }
     public static double rotatedY (int x,int y,double degree)
-    {       
+    {
         return Math.sin(Math.toRadians(degree))*x+Math.cos(Math.toRadians(degree))*y;
     }
     
     public static BufferedImage rotateFromCorner(BufferedImage oldImage,double degree)
     {
         BufferedImage newImage = new BufferedImage(oldImage.getWidth(), oldImage.getHeight(), oldImage.getType());
-        for(int i=0;i<newImage.getWidth();i++)
-        {
-            for(int j=0;j<newImage.getHeight();j++)
-            {
-                newImage.setRGB(i, j,0);            
-            }        
-        }
         for(int i=0;i<oldImage.getWidth();i++)
         {
             for(int j=0;j<oldImage.getHeight();j++)
             {
                 if(rotatedX(i,j,degree)>=0&&rotatedX(i,j,degree)<newImage.getWidth()&&rotatedY(i,j,degree)>=0&&rotatedY(i,j,degree)<newImage.getHeight())
-                newImage.setRGB((int)rotatedX(i,j,degree), (int)rotatedY(i,j,degree),oldImage.getRGB(i, j));            
-            }        
+                    newImage.setRGB((int)rotatedX(i,j,degree), (int)rotatedY(i,j,degree),oldImage.getRGB(i, j));
+            }
         }
         
         
         return newImage;
         
     }
-        public static BufferedImage rotateFromCenter(BufferedImage oldImage,double degree)
+    public static BufferedImage rotateFromCenter(BufferedImage oldImage,double degree)
     {
         BufferedImage newImage = new BufferedImage(oldImage.getWidth(), oldImage.getHeight(), oldImage.getType());
-        for(int i=0;i<newImage.getWidth();i++)
-        {
-            for(int j=0;j<newImage.getHeight();j++)
-            {
-                newImage.setRGB(i, j,0);            
-            }        
-        }
         for(int i=0;i<oldImage.getWidth();i++)
         {
             for(int j=0;j<oldImage.getHeight();j++)
             {
                 if(rotatedX(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getWidth()/2>=0&&rotatedX(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getWidth()/2<newImage.getWidth()&&rotatedY(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getHeight()/2>=0&&rotatedY(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getHeight()/2<newImage.getHeight())
-                newImage.setRGB((int)rotatedX(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getWidth()/2, (int)rotatedY(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getHeight()/2,oldImage.getRGB(i, j));            
-            }        
+                    newImage.setRGB((int)rotatedX(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getWidth()/2, (int)rotatedY(i-oldImage.getWidth()/2,j-oldImage.getHeight()/2,degree)+oldImage.getHeight()/2,oldImage.getRGB(i, j));
+            }
         }
         
         
@@ -80,25 +66,20 @@ public class ImageRotation {
     
     public static void main(String[] args) throws IOException {
         String path="C:\\Users\\dogan.cakir\\Documents\\NetBeansProjects\\ImageRotation\\burdur-golu.jpg";
-            BufferedImage img =ImageIO.read(new File(path));
-            Scanner in= new Scanner(System.in);
-            int degree=in.nextInt(); 
-            BufferedImage cornerRotated=rotateFromCorner(img, degree);
-            BufferedImage centralRotated=rotateFromCenter(img, degree);
-            JFrame frame = new JFrame();
-            frame.getContentPane().setLayout(new FlowLayout());
-            frame.getContentPane().add(new JLabel(new ImageIcon(img)));
-            frame.getContentPane().add(new JLabel(new ImageIcon(cornerRotated)));
-            frame.getContentPane().add(new JLabel(new ImageIcon(centralRotated)));
-            frame.pack();
-            frame.setVisible(true);
-            
+        BufferedImage img =ImageIO.read(new File(path));
+        Scanner in= new Scanner(System.in);
+        int degree=in.nextInt();
+        BufferedImage cornerRotated=rotateFromCorner(img, degree);
+        BufferedImage centralRotated=rotateFromCenter(img,degree);
+        JFrame frame = new JFrame();
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+        frame.getContentPane().add(new JLabel(new ImageIcon(cornerRotated)));
+        frame.getContentPane().add(new JLabel(new ImageIcon(centralRotated)));
+        frame.pack();
+        frame.setVisible(true);
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
-        
-        //test
-        
-        
     }
     
 }
--- test/Test1.java
+++ test/Test1.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 import imagerotation.ImageRotation;
 
 import org.junit.After;
@@ -32,69 +32,69 @@ public class Test1 {
     
     @Before
     public void setUp() {
-     x=1;
-     y=0;
+        x=1;
+        y=0;
     }
     
     @After
     public void tearDown() {
     }
-
+    
     @Test
-     public void testx90() {
-         assertEquals( 0, (int)ImageRotation.rotatedX(x, y, 90));
-     }
-     
-     @Test
-     public void testx180() {
-         assertEquals( -1, (int)ImageRotation.rotatedX(x, y, 180));
-     }
+    public void testx90() {
+        assertEquals( 0, (int)ImageRotation.rotatedX(x, y, 90));
+    }
+    
     @Test
-     public void testx270() {
-         assertEquals( 0, (int)ImageRotation.rotatedX(x, y, 270));
-     }
+    public void testx180() {
+        assertEquals( -1, (int)ImageRotation.rotatedX(x, y, 180));
+    }
     @Test
-     public void testx360() {
-         assertEquals( 1, (int)ImageRotation.rotatedX(x, y, 360));
-     }
+    public void testx270() {
+        assertEquals( 0, (int)ImageRotation.rotatedX(x, y, 270));
+    }
     @Test
-     public void testy90() {
-         assertEquals( 1, (int)ImageRotation.rotatedY(x, y, 90));
-     }
+    public void testx360() {
+        assertEquals( 1, (int)ImageRotation.rotatedX(x, y, 360));
+    }
     @Test
-     public void testy180() {
-         assertEquals( 0, (int)ImageRotation.rotatedY(x, y, 180));
-     }
+    public void testy90() {
+        assertEquals( 1, (int)ImageRotation.rotatedY(x, y, 90));
+    }
     @Test
-     public void testy270() {
-         assertEquals( -1, (int)ImageRotation.rotatedY(x, y, 270));
-     }
+    public void testy180() {
+        assertEquals( 0, (int)ImageRotation.rotatedY(x, y, 180));
+    }
     @Test
-     public void testy360() {
-         assertEquals( 0, ImageRotation.rotatedY(x, y, 360),3);
-     }
+    public void testy270() {
+        assertEquals( -1, (int)ImageRotation.rotatedY(x, y, 270));
+    }
     @Test
-     public void testy30() {
-         assertEquals( 0.866, ImageRotation.rotatedY(x, y, 30),3);
-     }
-     @Test
-     public void testx60() {
-         assertEquals( 0.866, ImageRotation.rotatedX(x, y, 60),3);
-     }
-     @Test
-     public void testy60() {
-         assertEquals( 0.5, ImageRotation.rotatedY(x, y, 60),1);
-     }
-     @Test
-     public void testx30() {
-         assertEquals( 0.5, ImageRotation.rotatedX(x, y, 30),1);
-     }
-      @Test
-     public void testx45() {
-         assertEquals( 0.707, ImageRotation.rotatedX(x, y, 45),3);
-     }
-     @Test
-     public void testy45() {
-         assertEquals( 0.707, ImageRotation.rotatedY(x, y, 45),3);
-     } 
+    public void testy360() {
+        assertEquals( 0, ImageRotation.rotatedY(x, y, 360),3);
+    }
+    @Test
+    public void testy30() {
+        assertEquals( 0.866, ImageRotation.rotatedY(x, y, 30),3);
+    }
+    @Test
+    public void testx60() {
+        assertEquals( 0.866, ImageRotation.rotatedX(x, y, 60),3);
+    }
+    @Test
+    public void testy60() {
+        assertEquals( 0.5, ImageRotation.rotatedY(x, y, 60),1);
+    }
+    @Test
+    public void testx30() {
+        assertEquals( 0.5, ImageRotation.rotatedX(x, y, 30),1);
+    }
+    @Test
+    public void testx45() {
+        assertEquals( 0.707, ImageRotation.rotatedX(x, y, 45),3);
+    }
+    @Test
+    public void testy45() {
+        assertEquals( 0.707, ImageRotation.rotatedY(x, y, 45),3);
+    }
 }
